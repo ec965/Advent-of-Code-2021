c8c81a62f41d48401816b1a1ed8c36914e0e0ec8
ec965 <enoch965@gmail.com>
2021-12-07 00:49:18 -0800

    finished day 4

--- day4/a.js
+++ day4/a.js
@@ -1,28 +1,98 @@
-const fs = require('fs');
+const fs = require("fs");
 
-function readInput(){
-  const input = fs.readFileSync('input.txt', {encoding: 'utf-8'})
-    .split('\n\n')
-    .filter(val => !!val);
+function readInput() {
+  const input = fs
+    .readFileSync("input.txt", { encoding: "utf-8" })
+    .split("\n\n")
+    .filter((val) => !!val);
   let [bingo, ...boards] = input;
-  bingo = bingo.split(',');
-  boards = boards.map(board => board.split('\n').filter(val => !!val));
+  bingo = bingo.split(",");
+  boards = boards.map((board) => board.split("\n").filter((val) => !!val));
 
   boards.forEach((board, i) => {
-    boards[i] = board.map(str => str.split(' ').filter(val => val !== ''));
-  })
+    boards[i] = board.map((str) => str.split(" ").filter((val) => val !== ""));
+  });
   return { bingo, boards };
 }
 
-function checkBoard(calledNums, board){
+function checkBoard(calledNums, board) {
   const numsSet = new Set(calledNums);
-  for(let i=0; i<board.length; i++){
-    for(let j=0; j<board[i].length; j++){
-      numSet.has(board[i][j]);
+
+  // check rows
+  for (let i = 0; i < board.length; i++) {
+    let lastFound = true;
+    for (let j = 0; j < board[i].length && lastFound; j++) {
+      if (!numsSet.has(board[i][j])) lastFound = false;
+    }
+    if (lastFound) return true; // we won!
+  }
+
+  // check cols
+  for (let i = 0; i < board.length; i++) {
+    let lastFound = true;
+    for (let j = 0; j < board[i].length && lastFound; j++) {
+      if (!numsSet.has(board[j][i])) lastFound = false;
+    }
+    if (lastFound) return true; // we won!
+  }
+
+  // check diagonals
+  let lastFound = true;
+  for (let i = 0; i < board.length && lastFound; i++) {
+    if (!numsSet.has(board[i][i])) lastFound = false;
+  }
+  if (lastFound) return true;
+
+  lastFound = true;
+  for (let i = 0; i < board.length && lastFound; i++) {
+    if (!numsSet.has(board[i][board.length - 1 - i])) lastFound = false;
+  }
+  if (lastFound) return true;
+
+  // we lost :(
+  return false;
+}
+
+function sumUnmarked(calledNums, board) {
+  const numsSet = new Set(calledNums);
+  let sum = 0;
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      if (!numsSet.has(board[i][j])) sum += parseInt(board[i][j]);
+    }
+  }
+  return sum;
+}
+
+function part1() {
+  const { bingo, boards } = readInput();
+  const calledNums = [];
+  for (let i = 0; i < bingo.length; i++) {
+    calledNums.push(bingo[i]);
+    for (let j = 0; j < boards.length; j++) {
+      if (checkBoard(calledNums, boards[j])) {
+        return sumUnmarked(calledNums, boards[j]) * bingo[i];
+      }
     }
   }
 }
 
-function part1(){
-  const { bingo, boards } =  readInput();
+function part2() {
+  const { bingo, boards } = readInput();
+  const calledNums = [];
+  const winningBoardSums = [];
+  const wonBoards = new Set();
+  for (let i = 0; i < bingo.length; i++) {
+    calledNums.push(bingo[i]);
+    for (let j = 0; j < boards.length; j++) {
+      if (!wonBoards.has(j) && checkBoard(calledNums, boards[j])) {
+        winningBoardSums.push(bingo[i] * sumUnmarked(calledNums, boards[j]));
+        wonBoards.add(j);
+      }
+    }
+  }
+  return winningBoardSums[winningBoardSums.length - 1];
 }
+
+console.log('part 1', part1());
+console.log("part 2", part2());
