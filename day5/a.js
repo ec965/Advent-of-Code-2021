0f3f27660c323aab9197968575d4544cb4fb1962
ec965 <enoch965@gmail.com>
2021-12-07 21:54:13 -0800

    fix input parse

--- day5/a.js
+++ day5/a.js
@@ -4,11 +4,12 @@ function getInput(){
   return fs.readFileSync('input.txt', {encoding: 'utf-8'})
     .split('\n')
     .filter(val => !!val)
-    .map(line => line.split(' -> ').map((val) => parseInt(val.replace(',', ''))));
+    .map(line => line.split(' -> ').map((val) => val.split(',')));
 }
 
 function part1(){
   const input = getInput();
+  return input;
 }
 
 console.log('part 1', part1());
